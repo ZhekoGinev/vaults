9d5fb44e001660fe51aa4c6067117c40c4b59327
Zheko Ginev <jginev@gmail.com>
2023-08-02 14:16:49 +0300

    Add files via upload

---
+++ linux/install_docker_ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+apt-cache policy docker-ce
+
+sudo apt install docker-ce
+
+sudo systemctl status docker
+
+sudo usermod -aG docker ${USER}
+
+su - ${USER}
+
+docker run hello-world
